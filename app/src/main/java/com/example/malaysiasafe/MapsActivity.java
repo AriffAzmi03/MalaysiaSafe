4dabaa2287e4a56872660f3d06200f40faf45118
ain <ainjabar869@gmail.com>
2025-01-15 00:11:46 +0800

    ain

--- app/src/main/java/com/example/malaysiasafe/MapsActivity.java
+++ app/src/main/java/com/example/malaysiasafe/MapsActivity.java
@@ -21,7 +21,7 @@ public class MapsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_maps);
 
         // Initialize the osmdroid configuration
         Context ctx = getApplicationContext();
